e85a8dd9c5fd468303c7cfa6a1311a9e112b30eb
Mehrdad Salehi <mhr.salehi@gmail.com>
2020-05-22 02:14:31 +0430

    initial commit

---
+++ src/main/java/com/mhr/xp/amqp/XpAmqpApplication.java
@@ -0,0 +1,13 @@
+package com.mhr.xp.amqp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpAmqpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XpAmqpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mhr/xp/amqp/XpAmqpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhr.xp.amqp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpAmqpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
